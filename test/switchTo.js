6a5a233d9e38c8db7ce52c186d2572833efa025e
Ryan Howard <ryanhoward@Ryans-MacBook-Pro.local>
2021-06-29 21:58:45 +0100

    added default content example

--- test/switchTo.js
+++ test/switchTo.js
@@ -6,3 +6,6 @@ await driver.switchTo().frame('frame-name');
 
 //switch to frame using web element
 await driver.switchTo().frame(driver.findElement(By.webLocator('frame-web-locator-property')));
+
+//switching back to first frame or main document
+await driver.switchTo().defaultContent()
