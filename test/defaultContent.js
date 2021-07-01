1196ec4666add12fc0a14eac59ede62c99057a56
Ryan Howard <ryan.howard@howqa.co.uk>
2021-07-01 16:01:37 +0100

    defaultContent update

--- test/defaultContent.js
+++ test/defaultContent.js
@@ -8,18 +8,25 @@ async function frames(){
     //navigate to our application
     await driver.get("https://the-internet.herokuapp.com/nested_frames");
 
+
+    //Navigate to top frame 
+    await driver.switchTo().frame(driver.findElement(By.name('frame-top')))
+    
     //Navigate to left Frame 
-    await driver.switchTo().frame(driver.findElement(By.name('frame-bottom')))
+    await driver.switchTo().frame(driver.findElement(By.name('frame-left')))
+    
+    //Use default content to navigate to top of tree structure
+    await driver.switchTo().defaultContent()
+
+    //Navigate to top frame 
+    await driver.switchTo().frame(driver.findElement(By.name('frame-top')))
 
     //return name of current frame 
     let currentFrame = await driver.executeScript("return self.name");
     //output name of current frame
     console.log(currentFrame);
 
-
     //close the browser
     await driver.quit();
-
-
 }
 frames()
